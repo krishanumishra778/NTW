80c72563bf5711db83f50431f42e872169a6c450
arunFullstackDeveloer <arundayma107@gmail.com>
2023-09-29 12:53:12 +0530

    add Home component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
+/** @format */
+
 import Header from "./Components/Header";
-import '../src/App.css'
+import "../src/App.css";
 import About from "./Components/About";
 import Services from "./Components/Services";
 import Founder from "./Components/Founder";
@@ -12,24 +14,17 @@ import Join from "./Components/Join";
 import Testimonials from "./Components/Testimonials";
 import Fotter from "./Components/Fotter";
 import Navbar from "./Components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./Components/Home";
 
 export default function App() {
   return (
     <>
-    <Navbar/>
-    <Header/>
-    <About/>
-    <Services/>
-    <Founder/>
-    <Subscription/>
-    <Creativeshowcase/>
-    <Testimonials/>
-    <Rating/>
-    <Trusted/>
-    <Contact/>
-    <Join/>
-    <Fotter/>
+      <Router>
+        <Routes>
+          {/* <Route path='/' element={<Home />} /> */}
+        </Routes>
+      </Router>
     </>
-    
-  )
+  );
 }
--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
         <img src="logo/next tech waves logo.png" alt='' className=" mx-auto" />
       </div>
 
-      <div className=" max-w-[1340px] px-[15px]  pt-9  mx-auto flex md:flex justify-between sm:flex-col-reverse xs:flex-col-reverse sm:flex-col-reverse lg:flex-row xl:flex-row 2xl:flex-row">
+      <div className=" max-w-[1340px] px-[15px]  pt-9  mx-auto flex md:flex justify-between  xs:flex-col-reverse sm:flex-col-reverse lg:flex-row xl:flex-row 2xl:flex-row">
         <div className="lg:w-[60%]">
           <h1 className="font-[600]  sm:text-tt  xs:text-mt md:text-title text-[#494949] inline leading-10">
             Unlock Your Digital Potential with Our Comprehensive Digital Solutions:
@@ -26,7 +26,7 @@ const Header = () => {
             digital requirements We can assist you in growing your business digitally regardless of how big
             or
             small it is.</p>
-          <button className="bg-[#00B2FF] rounded rounded-3xl   px-[20px] py-[5px]  sm:text-mp xs:text-tp md:text-p  text-[#fff]  my-5 hover:bg-[#00b3ffd8] transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:bg-[#00b3ffd8] duration-300 ...">Express
+          <button className="bg-[#00B2FF] rounded rounded-3xl   px-[20px] py-[5px]  sm:text-mp xs:text-tp md:text-p  text-[#fff]  my-5 hover:bg-[#00b3ffd8] transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300 ...">Express
             your needs</button>
         </div>
         <div className="my-6 lg:w-[50%] md:my-15  durat">
