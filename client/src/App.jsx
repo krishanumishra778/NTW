c2d624e9987b2fb11fdfc5f092f0f00ba1a38c1c
Ganeshhpatel <ganeshpatel2414@gmail.com>
2023-10-06 13:19:27 +0530

    some changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,11 +15,6 @@ import { Head_nav } from "./Components/layout/Head_nav";
 // import Fotter from "./Components/layout/Fotter";
 import { Settings } from "./Components/pages/Settings";
 import { Editprofile } from "./Components/pages/Editprofile";
-<<<<<<< HEAD
-import { Provider } from "react-redux";
-import { store } from "./Components/redux/Store";
-import { View } from "./Components/redux/View";
-=======
 import { Changepassword } from "./Components/pages/Changepassword";
 import { Pages } from "./Components/pages/Pages";
 import { useState } from "react";
@@ -28,48 +23,30 @@ import Join from "./Components/pages/Join";
 import Fotter from "./Components/layout/Fotter";
 
 
->>>>>>> 2e1ba08ada91bf4390464fcdee7b8ad02cbe6403
 
 
 
 export default function App() {
 
   // const location = useLocation();
-  
-  const [navurl , setNavurl] = useState()
+
+  const [navurl, setNavurl] = useState()
   // console.log(navurl);
 
-  
+
   return (
     <>
-      <Provider store={store}>
-        <View />
-        <Router>
 
-          <NavDemo />
-          <Head_nav />
 
-<<<<<<< HEAD
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about-us' element={<About />} />
-            <Route path='/services' element={<Services />} />
-            <Route path='/subscription' element={<Subscription />} />
-            <Route path='/portfolio' element={<Creativeshowcase />} />
-            <Route path='/signup' element={<User_SIgnup />} />
-            <Route path='/getotp' element={<Getotp />} />
-            <Route path='/login' element={< User_login />} />
-            <Route path="/settings" element={< Settings />} />
-            <Route path="/editprofile" element={< Editprofile />} />
-          </Routes>
-          <Fotter />
-        </Router>
-      </Provider>
-=======
+
+
+      <NavDemo />
+      <Head_nav />
+
       <Router>
-        {navurl !== "/sign-up" && <NavDemo setNavurl={setNavurl}/> }
-       
-       <Head_nav/>
+        {navurl !== "/sign-up" && <NavDemo setNavurl={setNavurl} />}
+
+        <Head_nav />
 
         <Routes>
           <Route path='/' element={<Home />} />
@@ -79,20 +56,20 @@ export default function App() {
           <Route path='/portfolio' element={<Creativeshowcase />} />
           <Route path='/sign-up' element={<User_SIgnup />} />
           <Route path='/get-otp' element={<Getotp />} />
-          <Route path='/log-in' element={< User_login/>} />
-          <Route path="/Settings" element={< Settings/>}/>
-          <Route path="/editprofile" element={ < Editprofile/>}/>
+          <Route path='/log-in' element={< User_login />} />
+          <Route path="/Settings" element={< Settings />} />
+          <Route path="/editprofile" element={< Editprofile />} />
           {/* <Route path="/changepassword" element={ < Changepassword/>}/> */}
-          <Route path="/changepassword" element={ < Changepassword/>}/> 
-          <Route path="/pages" element={ < Pages/>}/>
+          <Route path="/changepassword" element={< Changepassword />} />
+          <Route path="/pages" element={< Pages />} />
 
 
         </Routes>
-    <Contact/>
-    <Join/>
-    <Fotter/>
+        <Contact />
+        <Join />
+        <Fotter />
       </Router>
->>>>>>> 2e1ba08ada91bf4390464fcdee7b8ad02cbe6403
+
 
     </>
   );
--- client/src/Components/auth/User_SIgnup.jsx
+++ client/src/Components/auth/User_SIgnup.jsx
@@ -1,26 +1,26 @@
 /** @format */
 
-import React, { useState } from "react";
+import  { useState } from "react";
 import axios from "axios";
 import { AiFillEyeInvisible } from "react-icons/ai";
 
 // import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 // import jwt_decode from "jwt-decode";
 // import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 // import { Layout } from "../layout/Layout";
 import toast from "react-hot-toast";
-import { useDispatch } from "react-redux";
-import { loginuser } from "../redux/Action";
+
+
 
 export const User_SIgnup = () => {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
-  const [userData, setuserData] = useState({
+  const [userData, setUserData] = useState({
     name: "",
     email: "",
     company: "",
-    country: "",
+    country : "",
     password: "",
     email_verified: false, // Changed "false" to a boolean value
   });
@@ -110,7 +110,7 @@ export const User_SIgnup = () => {
                 <label
                   htmlFor='name'
                   className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
-                  Company Name
+                Company Name
                 </label>
                 <input
                   type='text'
@@ -132,7 +132,7 @@ export const User_SIgnup = () => {
                 <label
                   htmlFor='name'
                   className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
-                  Country Name
+                Country Name
                 </label>
                 <input
                   type='text'
@@ -147,55 +147,54 @@ export const User_SIgnup = () => {
                 />
               </div>
 
-              <div className="relative z-0 w-full mb-6 group">
+             
 
-                {/* ><<><><</></> */}
-                <div className='relative z-0 w-full mb-6 group'>
+              {/* ><<><><</></> */}
+              <div className='relative z-0 w-full mb-6 group'>
 
-                  <label
-                    htmlFor="password"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Your password
-                  </label>
-                  <input
-                    type="password"
-                    name="password"
-                    id="pwd"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 absolute h-8"
-                    placeholder="******"
-                    required
-                    onChange={inpHandler}
-                    value={userData.password}
-                    minLength={5}
-                    maxLength={10}
-                  />
-                  <AiFillEyeInvisible
-                    onClick={showPwd}
-                    className="relative left-[90%] top-2 cursor-pointer"
-                  />
-                </div>
-                {/* ................. */}
-                <input
-                  type="hidden"
-                  name="email_verified"
-                  value={userData.email_verified}
-                />
-
-                <button
-                  type="submit"
-                  className="text-white bg-blue-800 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-5 mb-5"
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  Submit
-                </button>
-                {/* <button className="bg-gray-50 hover:bg-[#f5f1f1] border border-gray-300  xs:text-tc md:text-c rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-[#494949]">
+                  Your password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="pwd"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 absolute h-8"
+                  placeholder="******"
+                  required
+                  onChange={inpHandler}
+                  value={userData.password}
+                  minLength={5}
+                  maxLength={10}
+                />
+                <AiFillEyeInvisible
+                  onClick={showpwd}
+                  className="relative left-[90%] top-2 cursor-pointer"
+                />
+              </div>
+              {/* ................. */}
+              <input
+                type="hidden"
+                name="email_verified"
+                value={userData.email_verified}
+              />
+
+              <button
+                type="submit"
+                className="text-white bg-blue-800 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-5 mb-5"
+              >
+                Submit
+              </button>
+              {/* <button className="bg-gray-50 hover:bg-[#f5f1f1] border border-gray-300  xs:text-tc md:text-c rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-[#494949]">
               < FcGoogle className="inline xs:text-tt md:text-t"/> Continue with google
               </button > */}
 
-                <div className="relative z-0 w-full mb-6 group text-center mt-2 text-blue-400">
-                  <Link to="/login">Already have an account?</Link>
-                </div>
-                </div>
+              <div className="relative z-0 w-full mb-6 group text-center mt-2 text-blue-400">
+                <Link to="/login">Already have an account?</Link>
+              </div>
 
             </form>
           </div>
