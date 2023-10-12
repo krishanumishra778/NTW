a7915c50ea3d8e67c89528f1bfcee41c3532f34e
shivi1122 <g7487294@gmail.com>
2023-10-12 16:48:25 +0530

    modify footer
    modify footer

--- client/src/Components/layout/Fotter.jsx
+++ client/src/Components/layout/Fotter.jsx
@@ -40,9 +40,9 @@ const Fotter = () => {
             <div className=" xs:max-w-[1300px]  mx-auto py-5 mt-6 md:mb-12 sm:grid grid-cols-2 gap-4 px-4" data-aos="fade-down">
                 <div >
                     <div>
-                        <img src="logo/next tech waves logo.png" />
+                        <img className='' src="logo/next tech waves logo.png" />
                     </div>
-                    <div className="flex justify-between items-center w-[50%] md:w-[40%] lg:w-[30%]  mt-2 pl-2 mx-auto sm:mx-0 sm:gap-2  ">
+                    <div className="flex justify-between mb-3 items-center w-[50%] md:w-[40%] lg:w-[30%]  mt-2 pl-2 mx-auto sm:mx-0 sm:gap-2  ">
                         <a className='' href="">
                             <img src="icons/insta.png" />
                         </a>
@@ -58,14 +58,14 @@ const Fotter = () => {
                     </div>
                 </div>
                 <div className=" sm:text-right  text-center">
-                        <form onSubmit={formHandler}>
-                    <div className="  py-1  mt-5 flex  ml-auto">
+                    <form onSubmit={formHandler}>
+                        <div className="  py-1  mt-5 flex  ml-auto">
                             <input type="email" className=" w-[70%] bg-[#F5F5F5] rounded-full pl-2  h-[35px] ml-1" placeholder="E-mail" onChange={inpHandler} name='email' value={email.email}
                                 required />
 
                             <button type='submit' className=" bg-[#00B2FF]  rounded-3xl text-[#fff]  ml-1 px-[20px] py-[5px]  sm:text-mp xs:text-tp md:text-p  hover:bg-[#00b3ffd8] " >Subscribe</button>
-                    </div>
-                        </form>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
