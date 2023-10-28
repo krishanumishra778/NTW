67f36372b056be44fb18cfb5d2a53296b422a2f3
Ganeshhpatel <ganeshpatel2414@gmail.com>
2023-10-28 11:33:35 +0530

    update admin panel pages

--- client/src/Components/pages/Adminpenal/TotalSubscriberActive.jsx
+++ client/src/Components/pages/Adminpenal/TotalSubscriberActive.jsx
@@ -25,7 +25,7 @@ export const TotalSubscriberActive = () => {
                     <img className='mx-auto' src="logo/next tech waves logo.png" alt='' />
                 </div>
 
-                <div className='overflow-y-scroll h-[80vh] '>
+                <div className='overflow-y-scroll h-[85.5vh] '>
                     <hr className=' bg-black  h-[2.5px]' />
                     <div className='flex m-8 gap-5 items-center '>
                         <div>
--- client/src/Components/pages/Services.jsx
+++ client/src/Components/pages/Services.jsx
@@ -5,7 +5,7 @@ const Services = () => {
  
     return (
         <>
-            <div className='bg-[url(images/bg-sub.png)] bg-no-repeat bg-cover' data-aos="fade-up" data-aos-duration="1000" data-aos-easing="ease">
+            <div className='bg-[url(images/bg-sub.png)] bg-no-repeat bg-cover' >
                 < div className=" max-w-[1300px] mx-auto xs:mt-5 lg:mt-10  py-8 md:px-[25px]" >
                     <div className=" xs:text-center md:text-left">
                         <h1 className="text-[#00B2FF] font-[600]  sm:text-tt  xs:text-mt md:text-title mb-4">
