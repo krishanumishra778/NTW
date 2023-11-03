d01f691ec432f07485eab4887140e8cbca7e2c98
shivi1122 <g7487294@gmail.com>
2023-11-03 13:47:53 +0530

    update
    update

--- client/src/Components/pages/VideoAdvertising.jsx
+++ client/src/Components/pages/VideoAdvertising.jsx
@@ -46,12 +46,12 @@ export const VideoAdvertising = () => {
     return (
         <div className="w-[98%] max-w-[1300px] mx-auto pt-16 text-center">
             <div className="relative">
-                <video ref={videoRef} width="90%" height="auto" className="rounded-3xl m-auto">
+                <video ref={videoRef} width="80%" height="100"  className="rounded-3xl m-auto">
                     <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4" />
                 </video>
                 <button className="absolute inset-0 flex items-center justify-center w-full h-full" onClick={togglePlayPause}>
                     {isPlaying ? (
-                        <img className=' w-[42px] h-[42px]' src="images/pause.png" alt="Pause" />
+                        <img className='w-[42px] h-[42px]' src="images/pause.png" alt="Pause" />
                     ) : (
                         <img className='w-[42px] h-[42px]' src="images/play.png" alt="Play" />
                     )}
